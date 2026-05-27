09142fb1ddd97416837d70806b8e07acc541efe2
Dishayadav1056 <dishaclarayadav@gmail.com>
2026-05-27 16:36:19 +0530

    Initial commit

---
+++ Fees.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const downloadBtn = document.querySelector(".download-btn");
+
+  downloadBtn.addEventListener("click", function () {
+    alert("Fee guide download will start soon.");
+  });
+});
